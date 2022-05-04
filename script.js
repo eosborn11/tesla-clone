ba0e18dd8126a8e58b4b29735a75df855d32a6c4
Mars Brown <meeshajean33@gmail.com>
2022-05-04 19:31:51 -0400

    DOM Elements linked

---
+++ script.js
@@ -0,0 +1,16 @@
+const fullpageEl = document.getElementById("fullpage");
+const menuBtn = document.querySelector(".menu_btn");
+const navigation = document.querySelector(".navigation");
+const navCloseBtn =  document.querySelector(".navigation_close_btn");
+
+const blurOverlay = document.querySelector(".blur_overlay");
+
+const IS_ACTIVE = "is--active";
+
+const toggleNavigation = () => {
+    navigation.classList.toggle(IS_ACTIVE);
+    blurOverlay.classList.toggle(IS_ACTIVE);
+    fullpageEl.classList.toggle("no-scroll");
+}
+
+const CLICK = "click";
